6d987d5e89016238e5bb111ae82686adfba4745a
Tomasz Nowacki <tomasz.nowacki@wsb.poznan.pl>
2026-02-28 12:05:11 +0100

    dodanie widoków kontakt i onas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+/* Route::get('/user/{id}', function (string $id) {
+    return 'User '.$id;
+}); */
+Route::get('/kontakt', function () {
+    return view('kontakt');
+});
+Route::get('/onas', function () {
+    return view('onas');
+});
