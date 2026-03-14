bfce84f251a599590440c811be592779ba9cc2ce
Tomasz Nowacki <tomasz.nowacki@wsb.poznan.pl>
2026-03-14 10:38:00 +0100

    dodanie menu nawigacji

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Route::get('/', function () {
 Route::get('/kontakt', function () {
     return view('kontakt');
 })->name('kontakt');
-Route::get('/onas', function () {
+Route::get('/o-nas', function () {
     $zadania =[
         'Zadanie 1',
         'Zadanie 2',
